097652d9b6e7355d358611fffd9baac6bd37c5a2
Кристина <kv6543778@gmail.com>
2025-09-18 04:40:39 +0500

    Add files via upload

---
+++ src/components/Modal.vue
@@ -0,0 +1,248 @@
+<template>
+  <div class="card-modal__overlay" :class="{open: isOpen}"  @click.self="closeModal()">
+    <div class="card-modal">
+      <div class="card-modal__header">
+        <h2 class="card-modal__header--title">Корзина</h2>
+        <span class="card-modal__header--close" @click="closeModal()">
+          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
+            <path d="M16.0035 13L25.3506 3.65245C25.6077 3.39513 25.7496 3.05184 25.75 2.68578C25.75 2.31952 25.6081 1.97582 25.3506 1.71891L24.5315 0.900011C24.274 0.642084 23.9307 0.500824 23.5642 0.500824C23.1984 0.500824 22.8551 0.642084 22.5976 0.900011L13.2504 10.247L3.90285 0.900011C3.64573 0.642084 3.30224 0.500824 2.93598 0.500824C2.57012 0.500824 2.22663 0.642084 1.96951 0.900011L1.15 1.71891C0.616667 2.25225 0.616667 3.11973 1.15 3.65245L10.4974 13L1.15 22.3472C0.892683 22.6049 0.751016 22.9482 0.751016 23.3142C0.751016 23.6803 0.892683 24.0236 1.15 24.2811L1.96931 25.1C2.22642 25.3577 2.57012 25.4992 2.93577 25.4992C3.30203 25.4992 3.64553 25.3577 3.90264 25.1L13.2502 15.7529L22.5974 25.1C22.8549 25.3577 23.1982 25.4992 23.564 25.4992H23.5644C23.9305 25.4992 24.2738 25.3577 24.5313 25.1L25.3504 24.2811C25.6075 24.0238 25.7494 23.6803 25.7494 23.3142C25.7494 22.9482 25.6075 22.6049 25.3504 22.3474L16.0035 13Z" fill="black"/>
+          </svg>
+        </span>
+      </div>
+      <div class="card-modal__body">
+        <div class="card-item">
+          <p class="card-item__title">
+            Ролл угорь стандарт
+          </p>
+          <div class="card-item__controls">
+            <div class="card-item__controls--price">
+              250 ₽
+            </div>
+            <button class="btn btn-outline"
+                    type="button"
+            >-</button>
+            <div class="card-item__controls--count">1</div>
+            <button class="btn btn-outline"
+                    type="button"
+            >+</button>
+          </div>
+        </div>
+        <div class="card-item">
+          <p class="card-item__title">
+            Ролл угорь стандарт
+          </p>
+          <div class="card-item__controls">
+            <div class="card-item__controls--price">
+              250 ₽
+            </div>
+            <button class="btn btn-outline"
+                    type="button"
+            >-</button>
+            <div class="card-item__controls--count">1</div>
+            <button class="btn btn-outline"
+                    type="button"
+            >+</button>
+          </div>
+        </div>
+        <div class="card-item">
+          <p class="card-item__title">
+            Ролл угорь стандарт
+          </p>
+          <div class="card-item__controls">
+            <div class="card-item__controls--price">
+              250 ₽
+            </div>
+            <button class="btn btn-outline"
+                    type="button"
+            >-</button>
+            <div class="card-item__controls--count">1</div>
+            <button class="btn btn-outline"
+                    type="button"
+            >+</button>
+          </div>
+        </div>
+        <div class="card-item">
+          <p class="card-item__title">
+            Ролл угорь стандарт
+          </p>
+          <div class="card-item__controls">
+            <div class="card-item__controls--price">
+              250 ₽
+            </div>
+            <button class="btn btn-outline"
+                    type="button"
+            >-</button>
+            <div class="card-item__controls--count">1</div>
+            <button class="btn btn-outline"
+                    type="button"
+            >+</button>
+          </div>
+        </div>
+        <div class="card-item">
+          <p class="card-item__title">
+            Ролл угорь стандарт
+          </p>
+          <div class="card-item__controls">
+            <div class="card-item__controls--price">
+              250 ₽
+            </div>
+            <button class="btn btn-outline"
+                    type="button"
+            >-</button>
+            <div class="card-item__controls--count">1</div>
+            <button class="btn btn-outline"
+                    type="button"
+            >+</button>
+          </div>
+        </div>
+
+      </div>
+      <div class="card-modal__footer">
+        <div class="card-modal__footer--price">
+          1250 ₽
+        </div>
+        <div class="card-modal__footer--controls">
+          <button class="btn btn-primary"
+                  type="button"
+          >
+            Оформить заказ
+          </button>
+          <button class="btn btn-outline"
+                  type="button"
+          >
+            Отмена
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const { isOpen } = defineProps({isOpen: Boolean})
+
+  const emit =defineEmits(['toggleModal'])
+  const closeModal = () => emit("toggleModal")
+
+</script>
+
+<style>
+
+.card-modal__overlay {
+  display: none;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.40);
+  align-items: center;
+  justify-content: center;
+}
+
+.card-modal__overlay.open {
+  display: flex;
+}
+
+.card-modal {
+  width: 100%;
+  max-width: 780px;
+  padding: 40px 45px;
+  border-radius: 5px;
+  background: #FFF;
+}
+
+.card-modal__header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 48px;
+}
+
+.card-modal__header--title {
+  color: #000;
+  font-size: 36px;
+  font-weight: 700;
+  margin: 0;
+}
+
+.card-modal__header--close {
+  cursor: pointer;
+}
+
+.card-modal__footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin-top: 50px;
+}
+
+.card-modal__footer--price {
+  border-radius: 5px;
+  background: #262626;
+  color: #FAFAFA;
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 20px;
+}
+
+.card-modal__footer--controls {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.card-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+
+.card-item:not(:last-child) {
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+  border-bottom: 1px solid #D9D9D9;
+}
+
+.card-item__title {
+  color: #000;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 32px;
+  margin: 0;
+}
+
+.card-item__controls {
+  display: flex;
+  align-items: center;
+}
+
+.card-item__controls button {
+  border-radius: 2px;
+  border: 1px solid #40A9FF;
+  background: #FFF;
+  color: #40A9FF;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+}
+
+.card-item__controls--price {
+  margin-right: 47px;
+  color: #000;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 32px;
+}
+
+.card-item__controls--count {
+  margin-left: 15px;
+  margin-right: 15px;
+}
+
+</style>
